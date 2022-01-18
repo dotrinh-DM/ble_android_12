3c6b8a9671cdefbca76b5203965257b745c2d498
Do Trinh <contact@dotrinh.com>
2022-01-18 21:05:39 +0900

    kết nối đến 1 thiết bị bất kì bằng cách tap

--- app/src/main/java/com/dotrinh/ble_android_12/MainActivity.java
+++ app/src/main/java/com/dotrinh/ble_android_12/MainActivity.java
@@ -10,7 +10,13 @@ import static com.dotrinh.ble_android_12.LogUtil.LogI;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
@@ -36,12 +42,14 @@ import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
     public ArrayList<BluetoothDevice> dataArr = new ArrayList<>();
     RecyclerView.Adapter adapter;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
     void startScan() {
         BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothAdapter = bluetoothManager.getAdapter();
         bluetoothAdapter.getBluetoothLeScanner().startScan(mScanCB);
     }
 
@@ -148,9 +156,104 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                LogI("click: " + dataArr.get(getAdapterPosition()).getName() + " : " + dataArr.get(getAdapterPosition()).getAddress());
+                mainBinding.progressBar.setVisibility(View.INVISIBLE);
+                bluetoothAdapter.getBluetoothLeScanner().stopScan(mScanCB);
+                connect(dataArr.get(getAdapterPosition()));
+                LogI("click stop: " + dataArr.get(getAdapterPosition()).getName() + " : " + dataArr.get(getAdapterPosition()).getAddress());
+            }
+        }
+    }
+
+    public BluetoothGatt bluetoothGatt;
+
+    public void connect(BluetoothDevice device) {
+        bluetoothGatt = device.connectGatt(this, false, gattCallback);
+    }
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            LogI("onConnectionStateChange status: " + status + " newState:" + newState);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    bluetoothGatt.discoverServices();
+                } else {
+                    gatt.close();
+                    LogI("GATT DISCONNECTED");
+                }
             }
         }
 
-    }
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                //DEVICE SERVICES
+                List<BluetoothGattService> gattServices = bluetoothGatt.getServices();
+                BluetoothGattService foundService = null;
+                for (BluetoothGattService gattServiceItem : gattServices) {
+                    UUID uuid = gattServiceItem.getUuid();
+                    LogI("UUID: " + uuid.toString());
+                    foundService = gattServiceItem;
+                }
+                if (foundService == null) {
+                    LogI("Ko thay service nao");
+                    return;
+                } else {
+                    LogI("da tim thay: " + gattServices.size() + " services");
+                }
+            } else {
+                LogI("loi 1");
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                LogI("doc thanh cong");
+            } else {
+                LogI("doc bi fail");
+            }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            switch (status) {
+                case BluetoothGatt.GATT_SUCCESS: {
+                    break;
+                }
+                case BluetoothGatt.GATT_REQUEST_NOT_SUPPORTED: {
+                    LogI("GATT_REQUEST_NOT_SUPPORTED");
+                    break;
+                }
+                case BluetoothGatt.GATT_READ_NOT_PERMITTED: {
+                    LogI("GATT_READ_NOT_PERMITTED");
+                    break;
+                }
+                case BluetoothGatt.GATT_WRITE_NOT_PERMITTED: {
+                    LogI("GATT_WRITE_NOT_PERMITTED");
+                    break;
+                }
+                default: {
+                    LogI("ghi bi fail");
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorWrite(gatt, descriptor, status);
+            LogI("onDescriptorWrite");
+            if (status != BluetoothGatt.GATT_SUCCESS) {
+                return;
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            byte[] data = characteristic.getValue();
+            LogI("onCharacteristicChanged");
+        }
+    };
 }
